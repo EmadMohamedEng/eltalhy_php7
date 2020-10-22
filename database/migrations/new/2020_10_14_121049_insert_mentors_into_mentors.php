1e32bd0061b7daf1768ea5280af708c5a7bb9c6d
omar tarek <omar.tarek@iVAS.local>
2020-10-22 12:43:37 +0200

    end point

--- database/migrations/new/2020_10_14_121049_insert_mentors_into_mentors.php
+++ database/migrations/new/2020_10_14_121049_insert_mentors_into_mentors.php
@@ -15,39 +15,39 @@ class InsertMentorsIntoMentors extends Migration
             [
                 [
                     'name' => 'العلامة الحبيب عبد القادر بن أحمد بن عبد الرحمن السقاف',
-                    'image' => 'uploads/mentor\\t1.jpg',
+                    'image' => 'uploads/mentor/t1.jpg',
                 ],
                 [
                   'name' => 'العلامة الحبيب عمر بن محمد بن سالم بن حفيظ بن الشيخ أبوبكر بن سالم',
-                  'image' => 'uploads/mentor\\t2.jpg',
+                  'image' => 'uploads/mentor/t2.jpg',
                 ],
                 [
                   'name' => 'العلامة الدكتور على جمعة مفتي الديار المصرية',
-                  'image' => 'uploads/mentor\\t6.jpg',
+                  'image' => 'uploads/mentor/t6.jpg',
                 ],
                 [
                   'name' => 'الشيخ المربي : السيّد محجوب أبوفارس الصفراني',
-                  'image' => 'uploads/mentor\\t3.jpg',
+                  'image' => 'uploads/mentor/t3.jpg',
                 ],
                 [
                   'name' => 'العلامة السيد الشيخ محمود افندي المجددي الخالدي النقشبندي',
-                  'image' => 'uploads/mentor\\t4.jpg',
+                  'image' => 'uploads/mentor/t4.jpg',
                 ],
                 [
                   'name' => 'العلامة الشيخ ابراهيم آل خليفة الاحسائي',
-                  'image' => 'uploads/mentor\\t7.jpg',
+                  'image' => 'uploads/mentor/t7.jpg',
                 ],
                 [
                   'name' => 'الحبيب أبوبكر بن علي العدني المشهور',
-                  'image' => 'uploads/mentor\\t8.jpg',
+                  'image' => 'uploads/mentor/t8.jpg',
                 ],
                 [
                   'name' => 'الحبيب علي زين العابدين بن عبد الرحمن الجفري',
-                  'image' => 'uploads/mentor\\t5.jpg',
+                  'image' => 'uploads/mentor/t5.jpg',
                 ],
                 [
                   'name' => 'الحبيب محمد بن عبد الرحمن السقاف',
-                  'image' => 'uploads/mentor\\t9.jpg',
+                  'image' => 'uploads/mentor/t9.jpg',
                 ],
 
             ]
